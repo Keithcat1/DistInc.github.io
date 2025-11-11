95f5b6b962c9ecbb5f5d9f3fe6b2ae726a7888da
Keith <keithwipf1@gmail.com>
2025-11-11 13:01:25 -0600

    Multiverse: fix max fields button not work

--- js/main/elementary/skyrmion.js
+++ js/main/elementary/skyrmion.js
@@ -271,8 +271,8 @@ function maxField(type, lims={}, ignoreUpgReq=false) {
 
 function maxBothFields() {
 	if (!tmp.ach[178].has) return;
-	maxField("pions", {}, true)
-	maxField("spinors", {}, true)
+	maxField("pions")
+	maxField("spinors")
 }
 
 function exportField(type) {
